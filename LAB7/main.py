c3b4a858a0431eee98130ca156ae8e567e2bcfd6
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-04-12 22:55:12 +0200

    commit 1
    Zadania 1 - 3

---
+++ LAB7/main.py
@@ -0,0 +1,25 @@
+import numpy
+
+# Zadanie 1
+array1 = numpy.arange(4, 84, 4)
+print(array1)
+
+# Zadanie 2
+array2 = numpy.arange(0.1, 5, 0.4, dtype='float64')
+print(array2)
+array2_int = array2.astype('int32')
+print(array2_int)
+
+# Zadanie 3
+
+def sparr(n):
+    array = numpy.empty((n, n))
+    pwr = 0
+    for i in range(n):
+        for j in range(n):
+            array[i, j] = 2 ** pwr
+            pwr += 1
+    return array
+
+
+print(sparr(3))
