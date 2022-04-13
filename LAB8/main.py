6dd35336af765b9b3e5056375161f1adc88d6f95
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-04-13 10:07:04 +0200

    Update main.py

--- LAB8/main.py
+++ LAB8/main.py
@@ -1,61 +1,82 @@
 import numpy
 
-a = numpy.array([20, 30, 40, 50])
-b = numpy.arange(4)
-c = a + b
-d = numpy.array([2.5, 65.2, 45.1, 2.6])
-print(c)
-print(b**3)
-e = d + a
-print(e)
+# Zadanie 1
+matrix1 = numpy.arange(3)
+matrix2 = numpy.arange(3)
+print(matrix1 * matrix2)
+print('===============')
 
+# Zadanie 2
+matrix3 = numpy.arange(9).reshape(3, 3)
+print(matrix3)
+print('Wartosci maksymalne w kolumnach:')
+print(matrix3.min(axis=0))
+print('Wartosci maksymalne w wierszach:')
+print(matrix3.min(axis=1))
+print('Wartosci minimalne w kolumnach:')
+print(matrix3.max(axis=0))
+print('Wartosci minimalne w wierszach:')
+print(matrix3.max(axis=1))
+print('===============')
 
-a = numpy.arange(12).reshape((3, 4))
+# Zadanie 3
+print(matrix1.dot(matrix2))
+print('===============')
+
+# Zadanie 4
+matrix4 = numpy.arange(3, dtype='int64')
+matrix5 = numpy.array([[4.6, 7.9, 2.3]])
+print(matrix4 * matrix5)
+print('===============')
+
+# Zadanie 5
+matrix6 = numpy.arange(6).reshape(2, 3)
+a = numpy.sin(matrix6)
 print(a)
-print('===')
-print(a.sum())
-print('===')
-print(a.sum(axis=0))
-print('===')
-print(a.sum(axis=1))
-print('===')
-print(a.cumsum(axis=1))
-print('===')
+print('===============')
 
+# Zadanie 6
+matrix7 = numpy.arange(10, 22, 2).reshape(2, 3)
+b = numpy.cos(matrix7)
+print(b)
+print('===============')
 
-a = numpy.arange(3)
-b = numpy.arange(3)
-c = a.dot(b)
-print(c)
-print('===')
-d = numpy.array([[1,2,4], [5, 2, 4]])
-e = numpy.array([[2,3], [6,3], [2, 4]])
-print(e.dot(d))
-print('===')
+# Zadanie 7
+print(a + b)
+print('===============')
 
+# Zadanie 8
+matrix8 = numpy.arange(9).reshape(3,3)
+for i in matrix8:
+    print(i)
+print('===============')
 
-f = numpy.arange(3)
-print(f)
-print('===')
-print(numpy.sqrt(f))
-print('===')
-print(numpy.cos(f))
-print('===')
+# Zadanie 9
+matrix10 = numpy.arange(9).reshape(3,3)
+for i in matrix8.flat:
+    print(i)
+print('===============')
 
-g = numpy.arange(6).reshape((3, 2))
-print(g)
+# Zadanie 10
+matrix9 = numpy.arange(81).reshape(9, 9)
+print(matrix9)
+matrix9a = matrix9.reshape(1,81)
+print(matrix9a)
+matrix9b = matrix9.reshape(81,1)
+print(matrix9b)
+matrix9c = matrix9.reshape(3,27)
+print(matrix9c)
+matrix9d = matrix9.reshape(27,3)
+print(matrix9d)
+# Wszystkie dzieliniki liczby 81
 
-for b in g.flat:
-    print(b)
+# Zadanie 11
+matrix11 = numpy.arange(12).reshape(3, 4)
+matrix12 = numpy.arange(12).reshape(4, 3)
+matrix13 = numpy.arange(12).reshape(2, 6)
 
+print(matrix11.flatten())
+print(matrix12.flatten())
+print(matrix13.flatten())
 
-print('===')
-
-h = numpy.arange(12)
-print(h)
-
-k = h.reshape((4,3))
-print(k)
-print('===')
-kT = k.T
-print(kT)
+# Tak.
